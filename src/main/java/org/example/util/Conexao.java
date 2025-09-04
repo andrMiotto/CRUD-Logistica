18ac0882128200402570cef5f9862c2c666b319d
andrMiotto <andre_lm_pereira@estudante.sesisenai.org.br>
2025-09-04 15:23:49 -0300

    teste

--- src/main/java/org/example/util/Conexao.java
+++ src/main/java/org/example/util/Conexao.java
@@ -1,17 +1,17 @@
 package org.example.util;
 
 
+import java.sql.Connection;
 import java.sql.DriverManager;
-
 import java.sql.SQLException;
 
-public class Connection {
+public class Conexao {
 
-    private static final String URL = "jdbc:mysql://localhost:3306/listaTelefonica?allowPublicKeyRetrieval=true&useSSL=false";
+    private static final String URL = "jdbc:mysql://localhost:3306/logistica?allowPublicKeyRetrieval=true&useSSL=false";
     private static final String USUARIO = "root";
     private static final String SENHA = "mysqlPW";
 
-    public static Connection conectar() throws SQLException {
-        return DriverManager.getConnection(URL, USUARIO, SENHA);
+    public static Conexao conectar() throws SQLException {
+        return (Conexao) DriverManager.getConnection(URL, USUARIO, SENHA);
     }
 }
